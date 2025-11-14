b37fd1fbfdcaae817145df71c5f35f9cc94ec5fc
phamtuthu <thupt@ghn.vn>
2025-11-14 13:06:06 +0700

    Define command list with descriptions
    Added a list of commands with descriptions for various teams and data reporting.

---
+++ commands.js
@@ -0,0 +1,41 @@
+export const COMMANDS = [
+  { command: "help", description: "Hiển thị menu trợ giúp" },
+
+  // Report
+  { command: "datanew", description: "Báo số lượng data buổi sáng" },
+  { command: "dataold", description: "Báo số lượng data buổi chiều" },
+
+  // Update tổng
+  { command: "updatenew", description: "Cập nhật Data New" },
+  { command: "updateold", description: "Cập nhật Data Old" },
+
+  // TEAM 1
+  { command: "3089136", description: "Phan Thanh Cường – Team 1" },
+  { command: "3110482", description: "Trần Thị Thu Mai – Team 1" },
+  { command: "3041313", description: "Đỗ Ngọc Trâm – Team 1" },
+  { command: "3089135", description: "Bùi Vĩnh Nguyên – Team 1" },
+  { command: "3125832", description: "Nguyễn Thị Anh – Team 1" },
+  { command: "3097094", description: "Lê Thị Vỹ Trinh – Team 1" },
+  { command: "3063800", description: "Nguyễn Thanh Tú – Team 1" },
+  { command: "3113236", description: "Phan Thị Đào – Team 1" },
+  { command: "3125839", description: "Nguyễn Hùng Thuận – Team 1" },
+
+  // TEAM 2
+  { command: "3053079", description: "Ngô Thuỳ Dương – Team 2" },
+  { command: "3061430", description: "Lê Anh Tuấn – Team 2" },
+  { command: "3115063", description: "Hồ Lam Nhiên – Team 2" },
+  { command: "3070887", description: "Lư Đức Hiển – Team 2" },
+  { command: "3108527", description: "Hà Sâm Minh – Team 2" },
+  { command: "3134239", description: "Nguyễn Hoàng Yến – Team 2" },
+  { command: "3111106", description: "Huỳnh Võ Anh Thư – Team 2" },
+  { command: "3097092", description: "Ngô Tuấn Kiệt – Team 2" },
+
+  // TEAM 3
+  { command: "3100229", description: "Lê Quốc Quân – Team 3" },
+  { command: "3066803", description: "Phan Nguyễn Diệu An – Team 3" },
+  { command: "3114284", description: "Nguyễn Lâm Trường – Team 3" },
+  { command: "3100526", description: "Đoàn Thị Trinh – Team 3" },
+  { command: "3065006", description: "Trần Thị Hằng – Team 3" },
+  { command: "3101076", description: "Lê Phạm Quỳnh Như – Team 3" },
+  { command: "3114283", description: "Bạch Tuấn Anh – Team 3" }
+];
